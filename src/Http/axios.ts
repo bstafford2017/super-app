e0b342743c5f537f4021663f63a5e83b8493e794
bstafford2017 <b.stafford2017@gmail.com>
2025-06-25 20:40:11 -0500

    Remove api key for old api

--- src/Http/axios.ts
+++ src/Http/axios.ts
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: '/api/stage-prod',
-  headers: {
-    'X-Api-Key': 'f/Lbqc8xujCtqRs8bZgXlA==xWx0MnDFYDiRDCo7',
-  },
   timeout: 5000,
 });
